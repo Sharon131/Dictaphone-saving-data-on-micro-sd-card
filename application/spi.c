2ce5671d84d38e6af8bdfd0bfd1ee148adb72537
Magdalena Pastuła <pastula.magdalena@gmail.com>
2019-08-30 14:38:47 +0200

    Update spi.c

--- application/spi.c
+++ application/spi.c
@@ -1,25 +1,27 @@
 #include "spi.h"
 
-#define SPI_BUFFER_SIZE		8
+#define SPI_BUFFER_SIZE		    8
 #define SPI_LSB_VALUE			1
 #define SPI_MSB_VALUE			128
+
+#define EMPTY_MSG_VALUE         0
 
 //TODO: zweryfikowac, czy poprawne wartosci sa wpisywane w elementy struktur we wszystkich Initach
 
 //another struct for all of these??
-static GPIO_TypeDef* SPI_MISO_Port;
-static uint16_t SPI_MISO_Pin;
-static GPIO_TypeDef* SPI_MOSI_Port;
-static uint16_t SPI_MOSI_Pin;
-static GPIO_TypeDef* SPI_SCK_Port;
-static uint16_t SPI_SCK_Pin;
-static GPIO_TypeDef* SPI_CS_Port;
-static uint16_t SPI_CS_Pin;
+static GPIO_TypeDef* SPI_MISO_Port = GPIOG;
+static uint16_t SPI_MISO_Pin = GPIO_PIN_9;
+static GPIO_TypeDef* SPI_MOSI_Port = GPIOG;
+static uint16_t SPI_MOSI_Pin = GPIO_PIN_14;
+static GPIO_TypeDef* SPI_SCK_Port = GPIOB;
+static uint16_t SPI_SCK_Pin = GPIO_PIN_2;
+static GPIO_TypeDef* SPI_CS_Port = GPIOC;
+static uint16_t SPI_CS_Pin = GPIO_PIN_8;
 
 static bool SPI_Polarity;
 static bool SPI_Phase;
-
-static char SPI_buffer[SPI_BUFFER_SIZE];
+static bool SPI_MSB_First;
+static uint8_t CRC_Polynomial = 7;
 
 static void __SPI_Init_MISO_MOSI_Pins(void){
 	GPIO_InitTypeDef GPIO_InitStruct_9;
@@ -32,9 +34,9 @@ static void __SPI_Init_MISO_MOSI_Pins(void){
   GPIO_InitStruct_9.Pull  = GPIO_PULLDOWN;
   GPIO_InitStruct_9.Speed = GPIO_SPEED_MEDIUM;
   
-  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct_9); 
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct_9);
    
-	GPIO_InitStruct_14.Pin   = GPIO_PIN_14;
+    GPIO_InitStruct_14.Pin   = GPIO_PIN_14;
   GPIO_InitStruct_14.Mode  = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct_14.Pull  = GPIO_PULLDOWN;
   GPIO_InitStruct_14.Speed = GPIO_SPEED_MEDIUM;
@@ -62,10 +64,10 @@ static void __SPI_Init_SCK_Pin(void){
 	__GPIOC_CLK_ENABLE();
 
 	GPIO_InitStruct_8.Pin   = GPIO_PIN_8;
-  GPIO_InitStruct_8.Mode  = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct_8.Speed = GPIO_SPEED_MEDIUM;
+    GPIO_InitStruct_8.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct_8.Speed = GPIO_SPEED_MEDIUM;
 	
-	if(SPI_Polarity == 0){											//i don't think it will be necessary
+	if(SPI_Polarity == 0){						//i don't think it will be necessary
 		GPIO_InitStruct_8.Pull  = GPIO_PULLDOWN;
 	} else{
 		GPIO_InitStruct_8.Pull  = GPIO_PULLUP;
@@ -75,15 +77,19 @@ static void __SPI_Init_SCK_Pin(void){
 }
 
 // More arg to add later maybe?: GPIO_TypeDef* MISO_Port, uint16_t MISO_Pin, GPIO_TypeDef* MOSI_Port, uint16_t MOSI_Pin, 
-//		GPIO_TypeDef* SCK_Port, uint16_t SCK_Pin, GPIO_TypeDef* CS_Port, uint16_t CS_Pin, 
+//		GPIO_TypeDef* SCK_Port, uint16_t SCK_Pin, GPIO_TypeDef* CS_Port, uint16_t CS_Pin, as structure
 
 void SPI_Init(bool Polarity, bool Phase){
-	
+	SPI_Polarity = Polarity;
+    SPI_Phase = Phase;
+
 	__SPI_Init_MISO_MOSI_Pins();
 	
 	__SPI_Init_CS_Pin();
 	
 	__SPI_Init_SCK_Pin();
+
+    //send clock, change mode to spi???
 }
 
 void __SPI_CopyString(char* Destination, char* Source, size_t SizeToCopy){
@@ -94,33 +100,47 @@ void __SPI_CopyString(char* Destination, char* Source, size_t SizeToCopy){
 	}
 }
 
-void __SPI_SendCharacter(char CharacterToSend){
-	
+void __SPI_SetPinTo();
+
+uint8_t __SPI_Trasmit(char CharacterToSend){
+	uint8_t ShiftRegister = CharacterToSend;
+
+
 	for(uint8_t index=0; index < SPI_BUFFER_SIZE; index++){
 		
-		if(SPI_Phase == 0) {
-			
+        //clock low
+        HAL_GPIO_Write_Pin(SPI_SCK_Port, SPI_SCK_Pin, GPIO_PIN_RESET);
+
+        //write
+        
+        if((ShiftRegister & SPI_MSB_VALUE) == SPI_MSB_VALUE){
+			HAL_GPIO_Write_Pin(SPI_MOSI_Port, SPI_MOSI_Pin, GPIO_PIN_SET);
 		} else {
-			
+			HAL_GPIO_Write_Pin(SPI_MOSI_Port, SPI_MOSI_Pin, GPIO_PIN_RESET);
 		}
-		//toggle clock ??
-		
-		if((CharacterToSend & SPI_MSB_VALUE) == SPI_MSB_VALUE){
-			//set high MOSI pin
-		} else {
-			//set low MOSI pin
-		}
-		
-		CharacterToSend = CharacterToSend << 1;
-		
-		//toggle clock
-		
-		//read
+
+        ShiftRegister = ShiftRegister << 1;
+
+        //delay until -> do implementacji
+
+		//clock high
+        HAL_GPIO_Write_Pin(SPI_SCK_Port, SPI_SCK_Pin, GPIO_PIN_SET);
+        
+        //read
+        if(HAL_GPIO_ReadPin(SPI_MISO_Port, SPI_MISO_Pin) == GPIO_PIN_SET){
+            ShiftRegister |= 1;
+        } else {
+            ShiftRegister |= 0;
+        }
+
+        //delay until
+
 	}
 	
+    return ShiftRegister;
 }
 
-char __SPI_ReadCharacter(void){
+char __SPI_ReadCharacter(void){//will not be needed. Delete
 	char CharacterRead = 0;
 	
 	for(uint8_t index=0; index < SPI_BUFFER_SIZE; index++){
@@ -133,10 +153,21 @@ char __SPI_ReadCharacter(void){
 	
 }
 
+//Add changing conn to spi in sd card interface
 void SPI_Send(char* MessageToSend){
-	;
+	
+    while(*MessageToSend != 0){
+        __SPI_Trasmit(*MessageToSend);
+        MessageToSend++;
+    }
 }
 
-void SPI_Read(char* MessageReceived){
-	;
+void SPI_Read(char* MessageReceived, size_t SizeOfBuffer){
+	uint16_t index = 0;
+
+    while(index != SizeOfBuffer){
+        *MessageReceived = __SPI_Trasmit(EMPTY_MSG_VALUE);
+        MessageReceived++;
+        index++;
+    }
 }
