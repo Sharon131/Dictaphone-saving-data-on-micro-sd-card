4dde1e89506ead5515ab388d50bc2801b26c7621
Magdalena Pastuła <pastula.magdalena@gmail.com>
2019-09-04 16:52:37 +0200

    Update sd_card.c

--- application/sd_card.c
+++ application/sd_card.c
@@ -4,9 +4,14 @@
 #define SPI_POL_IN_SDCARD_MODE	1
 #define SPI_PHA_IN_SDCARD_MODE	1
 
-#define SD_LENGTH_OF_COMMAND		6
+#define SD_LENGTH_OF_COMMAND	6
 
-#define SD_CMD0_CRC_VALUE				0x95
+#define SD_IDLE_STATE_VALUE		0xFF
+
+#define SD_DATA_BLOCK_SIZE		512
+
+#define SD_CMD0_CRC_VALUE		0x95
+#define SD_CMD8_CRC_VALUE		0x87
 
 /* Definitions for MMC/SDC command */
 #define CMD0    (0x40+0)    /* GO_IDLE_STATE */
@@ -25,6 +30,19 @@
 #define CMD55    (0x40+55)    /* APP_CMD */
 #define CMD58    (0x40+58)    /* READ_OCR */
 
+#define CMD17_18_24_DATA_TOKEN	0xFE
+#define CMD25_DATA_TOKEN		0xFC
+#define CMD25_STOP_TRAN_TOKEN	0xFD
+
+#define SD_SPI_MISO_PORT	GPIOG
+#define SD_SPI_MISO_PIN		GPIO_PIN_9
+#define SD_SPI_MOSI_PORT	GPIOG
+#define SD_SPI_MOSI_PIN		GPIO_PIN_14
+#define SD_SPI_SCK_PORT		GPIOB
+#define SD_SPI_SCK_PIN		GPIO_PIN_2
+#define SD_SPI_CS_PORT		GPIOC
+#define SD_SPI_CS_PIN		GPIO_PIN_8
+
 static uint8_t MessageBuffer[10];
 
 /* Sends clock signal 10 times to synchronize. */
@@ -40,7 +58,7 @@ void __SD_InitClockSignal(void){
 }
 
 /* Send command to change to SPI mode from SDIO mode. */
-void __SD_InitChangeToSPI_Mode(void){
+void __SD_InitChangeToSPI_Mode(void){//move to diskio file
 	
 	MessageBuffer[0] = CMD0;
 	MessageBuffer[1] = 0;
@@ -77,16 +95,38 @@ void SD_Init(void){
 
     SPI_Send(ByteReceived, 1);
 }
-void SD_SendCommand(char* CommandToSend, char* ArgsForCommand){
-	MessageBuffer[0] = CommandToSend;
-    MessageBuffer[1] = ArgsForCommand[0];
-    MessageBuffer[2] = ArgsForCommand[1];
-    MessageBuffer[3] = ArgsForCommand[2];
-    MessageBuffer[4] = ArgsForCommand[3];
-    MessageBuffer[5] = 0;
 
-    SPI_Send(MessageBuffer, SD_LENGTH_OF_COMMAND);
+void __SD_WaitForResponse(void){
+
+	while();//is pin set to 1 -> move it to SPI file?? rather not
 }
+
+
+void SD_SendCommand(uint8_t CommandToSend, uint32_t ArgsForCommand){
+	
+	SPI_Trasmit(CommandToSend);
+	SPI_Trasmit((uint8_t)(ArgsForCommand >> 24));
+	SPI_Trasmit((uint8_t)(ArgsForCommand >> 16));
+	SPI_Trasmit((uint8_t)(ArgsForCommand >> 8));
+	SPI_Trasmit((uint8_t)ArgsForCommand);
+	
+	if(CommandToSend == CMD0){
+		SPI_Trasmit(SD_CMD0_CRC_VALUE);
+	} else if (CommandToSend == CMD8) {
+		SPI_Trasmit(SD_CMD8_CRC_VALUE);
+	} else {
+		SPI_Trasmit(0);
+	}
+}
+
+bool SD_ReceiveDataBlock(uint8_t* Buffer, uint16_t NumberOfBytesToReceive){
+
+}
+
+bool SD_SendDataBlock(uint8_t* Buffer, uint16_t NumberOfBytesToSend){
+
+}
+
 
 void SD_ReadMessage(char* MessageRead){
 	SPI_Read(MessageRead, SD_LENGTH_OF_COMMAND);
