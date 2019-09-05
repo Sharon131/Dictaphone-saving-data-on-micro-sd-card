faf3fd9a46ee84db4f2fd96e2658683a28d98482
Magdalena Pastuła <pastula.magdalena@gmail.com>
2019-09-05 16:08:02 +0200

    First try of sd card module.

--- application/sd_card.h
+++ application/sd_card.h
@@ -6,9 +6,9 @@
 
 void SD_Init(void);
 
-void SD_SendCommand(uint8_t CommandToSend, uint32_t ArgsForCommand);
+uint8_t SD_SendCommand(uint8_t CommandToSend, uint32_t ArgsForCommand);
 
 bool SD_ReceiveDataBlock(uint8_t* Buffer, uint16_t NumberOfBytesToReceive);
-bool SD_SendDataBlock(uint8_t* Buffer, uint16_t NumberOfBytesToSend);
+bool SD_SendDataBlock(uint8_t* Buffer, uint16_t NumberOfBytesToSend, uint8_t Token);
 
 #endif
