ecc6115ebeb7980873ba81d0ac4822865b373104
Krystian Lalik <klalik27@gmail.com>
2019-09-07 14:33:11 +0200

    Added utility function that check byte order or reverse it. Needed for .wav file

--- application/core.c
+++ application/core.c
@@ -11,3 +11,26 @@ void CORE_ExitCriticalSection(void) {
 	__enable_irq();
 }
 
+uint32_t isLittleEndian(void)
+{
+	uint32_t test = 1;
+	uint8_t *p = (uint8_t*)&test;
+	
+	return p[0] == 1;
+}
+
+void reverseEndianness(void* value, uint32_t size)
+{
+	uint32_t i;
+	uint8_t result[32]; // room for 32 bytes
+	
+	for(i = 0; i < size; i++)
+	{
+		result[i] = ((uint8_t*)value)[size - i - 1];
+	}
+	
+	for(i = 0; i < size; i++)
+	{
+		((uint8_t*)value)[i] = result[i];		
+	}
+}
--- application/core.h
+++ application/core.h
@@ -5,5 +5,9 @@ void CORE_EnterCriticalSection(void);
 
 void CORE_ExitCriticalSection(void);
 
+uint32_t isLittleEndian(void);
+
+void reverseEndianness(void* value, uint32_t size);
+	
 #endif // _CORE_H_
 
